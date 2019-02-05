b6390c85ae2b841a451764a929db22622b0ebb12
fazur estrada ponce <limon-amargoverde15@hotmail.com>
2019-02-05 17:01:29 -0600

    Modificacion de Prueba.

---
+++ Ejemplo/Empleado.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejemplo
+{
+    class Empleado
+    {
+
+        public string nombre { get; set; }
+        public int edad { get; set; }
+        public string sexo { get; set; }
+
+        public Empleado() { }
+
+        public Empleado(string _nombre, int _edad, string _sexo)
+        {
+            this.nombre = _nombre;
+            this.edad = _edad;
+            this.sexo = _sexo;
+        }
+
+       
+
+    }
+}
--- Ejemplo/Program.cs
+++ Ejemplo/Program.cs
@@ -6,7 +6,16 @@ namespace Ejemplo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Empleado emp = new Empleado();
+
+            emp.nombre = "Fazur";
+            emp.edad = 19;
+            emp.sexo = "Masculino";
+
+            Console.WriteLine("El nombre es: " + emp.nombre + " \nLa edad es: " + emp.edad
+                + " \nEl sexo es: " + emp.sexo);
+
+            Console.ReadKey();
         }
     }
 }
